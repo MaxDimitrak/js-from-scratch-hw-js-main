a541b6b307d2ca6ebf516128f1fcd9d5cc7866a5
Maksim_Dimitrak <max.dimitrak@gmail.com>
2025-12-11 18:09:11 -0800

    solve: l05ex01

--- lesson-05/01.js
+++ lesson-05/01.js
@@ -15,5 +15,7 @@ const game = {
     gold: 250,
     lumber: 100,
   },
-  addResource() {}
+  addResource(resource, amount) {
+    game.resources[resource] ? this.resources[resource] += amount : console.log('Invalid resource');
+  }
 }
