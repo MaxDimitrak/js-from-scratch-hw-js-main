c7e0ee0e4291ac7a4fac1f214bd8e9522c6cbda0
Maksim_Dimitrak <max.dimitrak@gmail.com>
2025-12-12 11:41:55 -0800

    solve: l06ex01

--- lesson-06/01.js
+++ lesson-06/01.js
@@ -23,4 +23,12 @@ const oddNumbers = filter(numbers, (element, index) => {
 console.log(oddNumbers) // Должен вывести: [1, 3, 5]
 */
 
-const filter = () => {}
+const filter = (array, callback) => {
+  const filteredArr = [];
+  for (let i = 0; i < array.length; i++){
+    if (callback(array[i], i) === true){
+      filteredArr.push(array[i])
+    }
+  }
+  return filteredArr;
+}
